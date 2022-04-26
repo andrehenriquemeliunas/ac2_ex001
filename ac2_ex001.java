91b1a5cc9d3b851e3440b70d7bbc7c4f65d778a8
Andre Henrique <andremeliunas@hotmail.com>
2022-04-25 21:21:10 -0300

    finalizado

---
+++ ac2_ex001.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ac2_ex001{
+    public static void main( String[] args){
+        int idoso, def, ges;
+
+        Scanner s = new Scanner( System.in);
+        System.out.println("Você tem mais de 60 anos? Digite 1 para SIM, e 2 para NÃO");
+        idoso = s.nextInt();
+
+        System.out.println("Você possui algum tipo de deficiência? Digite 1 para SIM, e 2 para NÃO");
+        def = s.nextInt();
+
+        System.out.println("Você é gestante? Digite 1 para SIM, e 2 para NÃO");
+        ges = s.nextInt();
+
+        if(idoso == 1 || def == 1 || ges == 1){
+            System.out.print("Você PODE estacionar na vaga preferencial!");
+        }else{
+            System.out.print("Você NÃO pode estacionar na vaga preferencial!");
+        }
+
+        s.close();
+    }
+}

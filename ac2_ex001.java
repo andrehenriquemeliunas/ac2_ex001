d01d86d37c720973195eb3c446d5e2d9408feabf
Andre Henrique <andremeliunas@hotmail.com>
2022-04-25 21:56:20 -0300

    finalizado

--- ac2_ex001.java
+++ ac2_ex001.java
@@ -4,7 +4,7 @@ public class ac2_ex001{
         int idoso, def, ges;
 
         Scanner s = new Scanner( System.in);
-        System.out.println("Você tem mais de 60 anos? Digite 1 para SIM, e 2 para NÃO");
+        System.out.println("Você tem mais de 65 anos? Digite 1 para SIM, e 2 para NÃO");
         idoso = s.nextInt();
 
         System.out.println("Você possui algum tipo de deficiência? Digite 1 para SIM, e 2 para NÃO");

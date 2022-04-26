5be8d182d881bdb0de08a16d712c19a4d8936be2
Andre Henrique <andremeliunas@hotmail.com>
2022-04-25 22:00:11 -0300

    finalizado

--- ac2_ex001.java
+++ ac2_ex001.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class ac2_ex001{
     public static void main( String[] args){
-        int idoso, def, ges;
+        int idoso, def, ges;//Declaro as variáveis
 
         Scanner s = new Scanner( System.in);
-        System.out.println("Você tem mais de 65 anos? Digite 1 para SIM, e 2 para NÃO");
-        idoso = s.nextInt();
+        System.out.println("Você tem mais de 65 anos? Digite 1 para SIM, e 2 para NÃO");//Verifico se o usuário tem mais de 65 anos
+        idoso = s.nextInt();//Salvo a resposta do usuário
 
-        System.out.println("Você possui algum tipo de deficiência? Digite 1 para SIM, e 2 para NÃO");
-        def = s.nextInt();
+        System.out.println("Você possui algum tipo de deficiência? Digite 1 para SIM, e 2 para NÃO");//Verifico se o usuário tem alguma deficiência
+        def = s.nextInt();//Salvo a resposta do usuário
 
-        System.out.println("Você é gestante? Digite 1 para SIM, e 2 para NÃO");
-        ges = s.nextInt();
+        System.out.println("Você é gestante? Digite 1 para SIM, e 2 para NÃO");//Verifico se o usuário é gestante
+        ges = s.nextInt();//Salvo a resposta do usuário
 
-        if(idoso == 1 || def == 1 || ges == 1){
-            System.out.print("Você PODE estacionar na vaga preferencial!");
+        if(idoso == 1 || def == 1 || ges == 1){//Condicional na qual todas tem que ser verdadeiras
+            System.out.print("Você PODE estacionar na vaga preferencial!");//Se a condição for satisfeita
         }else{
-            System.out.print("Você NÃO pode estacionar na vaga preferencial!");
+            System.out.print("Você NÃO pode estacionar na vaga preferencial!");//Se a condição não for satisfeita
         }
 
         s.close();
